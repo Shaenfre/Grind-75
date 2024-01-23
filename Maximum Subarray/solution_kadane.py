0f09c67531435fe3661945d919ed8eb8809b496b
Shubham <rajs93222@gmail.com>
2024-01-23 23:01:43 +0530

    maxSubArray

---
+++ Maximum Subarray/solution_kadane.py
@@ -0,0 +1,17 @@
+'''
+time complexity O(n)
+space complexity O(1)
+
+to do: implement in dp way
+'''
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sum, ans = 0, -1000000
+        for n in nums:
+            sum += n
+            ans = max(sum, ans)
+            if sum < 0:
+                sum = 0
+        
+        return ans
