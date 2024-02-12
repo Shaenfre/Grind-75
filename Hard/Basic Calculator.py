ac799aad87ddae3a787f764525bc73427f0a6488
Shubham <rajs93222@gmail.com>
2024-02-12 22:46:55 +0530

    n queens and basic calculator

---
+++ Hard/Basic Calculator.py
@@ -0,0 +1,41 @@
+'''
+Time complexity O(n)
+Space complexity O(n)
+'''
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        sign = 1
+        res = 0
+        current = 0
+        s = "(" + s + ")"
+
+        for c in s:
+            if c == " ":
+                continue
+            elif c == "+":
+                res += sign * current
+                current = 0
+                sign = 1
+            elif c == "-":
+                res += sign * current
+                current = 0
+                sign = -1
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ")":
+                res += sign * current
+                sign = stack.pop()
+                res *= sign
+                current = stack.pop()
+                res += current 
+                current = 0
+                sign = 1
+            else:
+                current = current * 10 + int(c)
+            # print(stack)
+
+        return res
---
+++ Hard/N Queens.py
@@ -0,0 +1,33 @@
+'''
+Time complexity O(n!)
+Space complexity O(n)
+'''
+class Solution:
+    def solveNQueens(self, N: int) -> List[List[str]]:
+        ans = []
+
+        def tryRow(row, board, column_used, diag1_used, diag2_used):
+            if row == N:
+                current = []
+                for r in board:
+                    current.append("".join(r))
+                ans.append(current)
+                return
+
+            for col in range(N):
+                if col not in column_used and row + col not in diag1_used and row - col not in diag2_used:
+                    board[row][col] = "Q"
+                    column_used.add(col)
+                    diag1_used.add(row + col)
+                    diag2_used.add(row - col)
+                    tryRow(row + 1, board, column_used, diag1_used, diag2_used)
+                    board[row][col] = "."
+                    column_used.remove(col)
+                    diag1_used.remove(row + col)
+                    diag2_used.remove(row - col)
+
+        board = [["."] * N for _ in range(N)]
+        tryRow(0, board, set(), set(), set())
+        return ans
+
+        
